4569cc103013557a807f236455dd1db8a2cb175e
irov <irov@IROV-WOK>
2017-03-15 17:53:28 +0300

    init

---
+++ SmartcoolerDevCPP/SmartcoolerDevCPP/SmartcoolerDevCPP.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SmartcoolerDevCPP.ino
+ Created:	3/15/2017 5:52:58 PM
+ Author:	irov
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
